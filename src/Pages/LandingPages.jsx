66d07cfbcc54956aadc3b36a0d934399c3fc6554
saswatjit ptra <saswatp813@gmail.com>
2025-04-22 22:15:22 +0530

    upadating the landing page import statment

--- src/Pages/LandingPages.jsx
+++ src/Pages/LandingPages.jsx
@@ -3,7 +3,7 @@ import Navbar from "../components/navbar/Navbar";
 import Hero from "../components/sections/Hero";
 import Unique from "../components/sections/Unique";
 import Juices from "../components/sections/Juices";
-import FreshJuice from "../components/sections/freshJuice";
+import FreshJuice from "../components/sections/FreshJuice";
 import Footer from "../components/sections/Footer";
 import SubscribeSection from "../components/sections/SubscribeSection";
 
